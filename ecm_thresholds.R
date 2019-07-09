e97641d30565cd97c8bca7b59789396233e8c034
Ajay Pillarisetti <ajaypillarisetti@berkeley.edu>
2019-07-08 22:51:00 -0700

    Update PM thresholds

--- ecm_thresholds.R
+++ ecm_thresholds.R
@@ -1,5 +1,5 @@
 #set QA-QC parameters
-flow_thresholds <- c(0.285, 0.315)
+flow_thresholds <- c(0.25, 0.35)
 flow_cutoff_threshold <- 0.10
 flow_rolling_sd_threshold <-  0.2
 flow_min_threshold <- 0.10
@@ -7,10 +7,10 @@ flow_max_threshold <- 0.50
 
 sample_duration_thresholds <- c(1440*.90,1440*1.10)
 
-bl_shift_threshold <- 10
+bl_shift_threshold <- 30
 
-neph_neg_threshold <- 10
-neph_neg_magnitude_threshold <- -10
+neph_neg_threshold <- 20
+neph_neg_magnitude_threshold <- -20
 
 inlet_pressure_threshold <- 5
 
@@ -20,4 +20,4 @@ rh_threshold <- 85
 
 accel_compliance_threshold <- 0.02 #Compliance flag
 window_width <- 20
-overall_compliance_threshold <- 0.4 #If more than this fraction of samples is not categorize as being compliant, run is flagged.  Includes nighttime data.
+overall_compliance_threshold <- 0.2 #If more than this fraction of samples is not categorize as being compliant, run is flagged.  Includes nighttime data.
