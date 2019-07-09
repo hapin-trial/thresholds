61dd80c9cf47f4e7fa5ddf62734382869038e006
Ajay Pillarisetti <ajaypillarisetti@berkeley.edu>
2019-07-08 22:49:01 -0700

    Update CO thresholds

--- lascar_thresholds.R
+++ lascar_thresholds.R
@@ -2,10 +2,10 @@
 
 max_1hr_avg_threshold <- 87 #ppm (uncalibrated)
 Daily_avg_threshold <- 6.1
-Daily_stdev_threshold <- 10
+Daily_stdev_threshold <- 10/2
 elevated_night_threshold <- 6.1
 bl_shift_threshold <- 5
-sample_duration_thresholds <- c(1440*.90,1440*1.10)
+sample_duration_thresholds <- c(1440*.90,1440*2.0)
 
 
 
