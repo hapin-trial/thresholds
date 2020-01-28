0b7374ad77290d94cd44dc366abf77b8ae05c8b9
Ricardo Piedrahita <ricpie@gmail.com>
2020-01-28 18:48:14 -0500

    Update lascar_thresholds.R
    Added calibration slope thresholds

--- lascar_thresholds.R
+++ lascar_thresholds.R
@@ -7,6 +7,11 @@ elevated_night_threshold <- 6.1
 bl_shift_threshold <- 5
 sample_duration_thresholds <- c(1440*.90,1440*2.0)
 
+slope_threshold <- c(.25,2)#Lascar ppm/actual ppm
+intercept_threshold <- c(-5,5)
+stdev0_threshold <- 3
+CoV_threshold <- c(0,0.25)
+spanpercent_threshold <- c(-.66,.66)
 
 
 
