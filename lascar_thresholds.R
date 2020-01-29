f61465b3c16fc27710ff65b349d66851534a8e2a
Ricardo Piedrahita <ricpie@gmail.com>
2020-01-29 16:54:02 -0500

    Update lascar_thresholds.R
    tighter stdevzero threshold

--- lascar_thresholds.R
+++ lascar_thresholds.R
@@ -9,7 +9,7 @@ sample_duration_thresholds <- c(1440*.90,1440*2.0)
 
 slope_threshold <- c(.25,2)#Lascar ppm/actual ppm
 intercept_threshold <- c(-5,5)
-stdev0_threshold <- 3
+stdev0_threshold <- 2
 CoV_threshold <- c(0,0.25)
 spanpercent_threshold <- c(-.66,.66)
 
