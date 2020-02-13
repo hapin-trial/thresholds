2af8829de71508ccca9712b244aca17a3a306d10
Ricardo Piedrahita <ricpie@gmail.com>
2020-02-13 16:32:05 -0700

    Update lascar_thresholds.R
    Removed long duration flag, we can truncate if files are too long

--- lascar_thresholds.R
+++ lascar_thresholds.R
@@ -5,7 +5,7 @@ Daily_avg_threshold <- 6.1
 Daily_stdev_threshold <- 10/2
 elevated_night_threshold <- 6.1
 bl_shift_threshold <- 5
-sample_duration_thresholds <- c(1440*.90,1440*2.0)
+sample_duration_thresholds <- c(1440*.90,1440*100.0) #too long is never an issue as it is truncated.
 
 slope_threshold <- c(.25,2)#Lascar ppm/actual ppm
 intercept_threshold <- c(-5,5)
