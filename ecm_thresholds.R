61888b531939ab9496fa80f27eaa43878eeea449
Ricardo Piedrahita <ricpie@gmail.com>
2020-10-06 11:42:09 -0600

    Update ecm_thresholds.R
    Loosened flow and duration restrictions, made the percent deviations consistent with the duration thresholds.
    We will no longer use the flow_max and flow_min flow flags.  Noted as much, retained them in case of legacy code.

--- ecm_thresholds.R
+++ ecm_thresholds.R
@@ -1,11 +1,14 @@
 #set QA-QC parameters
-flow_thresholds <- c(0.25, 0.35)
-flow_cutoff_threshold <- 0.10
-flow_rolling_sd_threshold <-  0.2
-flow_min_threshold <- 0.10
-flow_max_threshold <- 0.50	
 
-sample_duration_thresholds <- c(1440*.90,1440*1.10)
+#sample_duration_thresholds <- c(1440*.90,1440*1.10)
+sample_duration_thresholds <- c(1200,1680)# +/- 4 hours
+
+#flow_thresholds <- c(0.25, 0.35)
+flow_thresholds <- c(0.2, 0.4)
+flow_cutoff_threshold <- sample_duration_thresholds[1]/1440
+flow_rolling_sd_threshold <- sample_duration_thresholds[1]/1440
+flow_min_threshold <- 0.10 #Cease usage, somewhat redundant and unnecessary
+flow_max_threshold <- 0.50	#Cease usage, somewhat redundant and unnecessary
 
 bl_shift_threshold <- 30
 
