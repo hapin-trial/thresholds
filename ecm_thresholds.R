c20c21a6c8fc2e775d9edc6fdb08e4ea8c4009a0
Ricardo Piedrahita <ricpie@gmail.com>
2020-10-06 15:03:40 -0600

    Update ecm_thresholds.R
    there...

--- ecm_thresholds.R
+++ ecm_thresholds.R
@@ -6,7 +6,7 @@ sample_duration_thresholds <- c(1200,1680)# +/- 4 hours
 #flow_thresholds <- c(0.25, 0.35)
 flow_thresholds <- c(0.2, 0.4)
 flow_cutoff_threshold <- (1440-sample_duration_thresholds[1])/1440 #Was 0.1
-flow_rolling_sd_threshold <- flow_cutoff_threshold #Was 0.1?
+flow_rolling_sd_threshold <- flow_cutoff_threshold #Was 0.2
 flow_min_threshold <- 0.10 #Cease usage, somewhat redundant and unnecessary
 flow_max_threshold <- 0.50	#Cease usage, somewhat redundant and unnecessary
 
