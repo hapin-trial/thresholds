6a9147d6d489fde2934fc7c4daa1506a2313d817
Ajay Pillarisetti <ajaypillarisetti@gmail.com>
2020-11-07 17:11:24 -0500

    Update flow flags, datetime calc, and ingest function
    Danger.

--- ecm_functions.R
+++ ecm_functions.R
@@ -246,8 +246,195 @@ ecm_ingest <- function(file, tz="UTC", shiny=TRUE, output=c('raw_data', 'meta_da
   }
 }
 
+ecm_ingest_v2 <- function(file, tz="UTC", shiny=TRUE, output=c('raw_data', 'meta_data'), dummy='dummy_meta_data'){
+  
+  dummy_meta_data <- get(dummy, envir=.GlobalEnv)
+  
+  if(!shiny){ecm_copy(file)}
+  
+  unprocessed <- read.csv(file, nrow=1, header=F, stringsAsFactor=F)$V1
+  
+  if(unprocessed=="?;m" || dim(fread(file, fill = TRUE))[1] < 30){
+    base::message(basename(file), " is not processed. Process and try again.")
+    rm(unprocessed)
+    #add "fake" metadata
+    metadata <- dummy_meta_data
+    metadata$file <- file
+    metadata$qa_date = as.Date(Sys.Date())
+    metadata$flags <- 'file not processed'
+    metadata[, c('smry', 'analysis') := NULL]
+    return(list(raw_data=NULL, meta_data=metadata))
+  }else{
+    rm(unprocessed)
+    #separate out raw, meta, and sensor data
+    raw_data <- fread(file, skip = 29) 
+
+    if(nrow(raw_data) < 300){
+      message(basename(file), " has less than 300 rows of data.")
+      return(NULL)
+    }else{
+
+    
+    #vector of column names from a "good" file
+    full_columns_list <- c("Date", "Time", "RH-Corrected Nephelometer", "RH-Corrected Nephelometer HR", "Temp", "RH", "Battery", "Inlet Press", "Orifice Press", "Flow", "X-axis", "Y-axis", "Z-axis", "Vector Sum Composite", "ShutDownReason", "Wearing Compliance", "ValidityWearingCompliance validation")
+    
+    #columns are not necessarily in order, so we need to extract column names from the file to prevent errors
+    #first, we find the row with the header - the first row with "Date,Time"
+    header_id <- min(grep('Date,Time', as.character(read.delim(file, nrows=30, header=F, stringsAsFactor=F)$V1)))
+    #next, we import that row
+    header_names <- as.character(read.csv(file, skip=header_id,nrow=1, header=F, stringsAsFactor=F))
+    
+    #some files have a lot of extra columns, likely a byproduct of being opened in Excel and saved
+    #others do not have enough columns, inexplicably. 
+    #This tries to cope with that. 
+    skipflag <- 0
+    
+    #Deals with another edge case that shifts the header row (e.g.~/Box Sync/ecm_test/12108_PEM_ECM123_060_25072017.csv")
+    if(ncol(raw_data)>=17 & grepl(header_names[3],"μg/m³")){
+      skipflag <- 1
+      header_names <- as.character(fread(file, skip=header_id-skipflag, nrows=1, header=F))[1:17]
+      raw_data <- raw_data[,c(1:17)]
+      setnames(raw_data, header_names)
+    }else if(ncol(raw_data)>=17){
+      raw_data <- raw_data[,c(1:17)]
+      setnames(raw_data, header_names)
+    }else{
+      setnames(raw_data, header_names)
+      missing_cols <- full_columns_list[!(full_columns_list %in% header_names)]
+      raw_data[, c(missing_cols) := NA]
+    }
+    
+    setcolorder(raw_data, full_columns_list)
+    
+    setnames(raw_data, c('date', 'time', 'rh_cor_neph', 'rh_cor_neph_hr', 'temp', 'rh','batt',  'inlet_pres', 'orifice_pres', 'flow', 'acc_x',  'acc_y',  'acc_z',  'acc_comp', 'shutdown_reason',  'wearing_compliance', 'validity_wearing_comp'))
+    
+    raw_data[, flow:=as.numeric(flow)]
+    
+    meta_data <- as.data.table(read.csv(file, nrow=14-skipflag, header=F, stringsAsFactor=F))
+    meta_data_dutycycle <- as.data.table(read.csv(file, skip = 11, nrows = 3, header = F, stringsAsFactors = F))
+    
+    #for the time being, we use precious little of this
+    sensor_data <- as.data.table(read.csv(file, skip=13-skipflag, nrow=10, header=T, stringsAsFactor=F))[, c(1:6)]
+    if (skipflag<1){sensor_data <- sensor_data[Sensor!='0.3']}
+    setnames(sensor_data, c('sensor', 'slope', 'offset', 'log_interval', 'high_target', 'low_target'))
+    
+    #deal with dates and times
+    raw_data[, datetime:=paste(date, time, sep=" ")]
+    raw_data[, date:=NULL]
+    raw_data[, time:=NULL]
+
+    if(all(grepl('[0-9]{3}20[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}', raw_data[, datetime]))){
+      raw_data[, datetime := paste('0', datetime, sep="")]
+    }
+    
+    #since samples are short, simply "guessing" doesn't work -- for example, some files were taken between 8/8 and 8/10.
+    #Use a simple algorithm to determine, of the three DT options below, which one has the smallest overall range
+    date_formats <- suppressWarnings(
+      melt(
+        data.table(
+          dmy = raw_data[, as.numeric(difftime(max(dmy_hms(datetime, tz=tz)), min(dmy_hms(datetime, tz=tz))))],
+          ymd = raw_data[, as.numeric(difftime(max(ymd_hms(datetime, tz=tz)), min(ymd_hms(datetime, tz=tz))))],
+          mdy = raw_data[, as.numeric(difftime(max(mdy_hms(datetime, tz=tz)), min(mdy_hms(datetime, tz=tz))))]
+        )
+      )
+    )
+    
+      #extract download date from metadata
+      download_date <- mdy(meta_data[2,V2])
+      #get the max date in the raw_data for which acc_comp is not missing (i.e. data is still logging, not some weird
+      #post sampling artifact. May not be right.)
+      raw_data[, `:=`(SUM = sum(.SD, na.rm=TRUE)),.SDcols=c(1:12),by=1:nrow(raw_data)] 
+      max_file_date <- raw_data[SUM!=0, max(datetime)]
+
+      #create revised date formats table: look for difference between DATE of download and DATE of the file's max timestamp
+      revised_date_formats <- melt(
+        data.table(
+          dmy = as.numeric(difftime(download_date, as.Date(suppressWarnings(dmy_hms(max_file_date))), unit = 'hours')),
+          ymd = as.numeric(difftime(download_date, as.Date(suppressWarnings(ymd_hms(max_file_date))), unit = 'hours')),
+          mdy = as.numeric(difftime(download_date, as.Date(suppressWarnings(mdy_hms(max_file_date))), unit = 'hours'))
+        ), measure.vars = c('dmy', 'ymd', 'mdy')
+      )
+      revised_date_formats[, value := abs(value)]
+      # revised_date_formats[value == revised_date_formats[, min(value, na.rm = T)], variable]
+      date_formats  <-  revised_date_formats
+      
+      date_format <- as.character(date_formats[value==date_formats[!is.na(value),min(value)], variable])
+      
+      if(date_format=="mdy"){raw_data[, datetime:=mdy_hms(datetime, tz=tz)]} else
+        if(date_format=="ymd"){raw_data[, datetime:=ymd_hms(datetime, tz=tz)]} else
+          if(date_format=="dmy"){raw_data[, datetime:=dmy_hms(datetime, tz=tz)]}
+      
+      #remove columns we don't need
+      raw_data[, wearing_compliance:=NULL]
+      raw_data[, validity_wearing_comp:=NULL]
+      raw_data[, SUM := NULL]      
+      
+      #find large breaks in data, remove everything before
+      #per JL's suggestion
+      #AP make seconds explicit, coerce to numeric
+      #add acc_comp to ignore lines with USB stopping logging
+      
+      #Drop rows farther than 1 day away from the median time.  Alternative is do use a dropline check below. Perhaps both, loosening the time restriction on the latter? 
+      median_time = median(raw_data$datetime,na.rm = TRUE)
+      raw_data <- raw_data[abs(difftime(raw_data$datetime,median_time))<86400,]
+      
+      #is 10 seconds really the right threshold??
+      # drop_line_check <- raw_data[acc_comp!="", as.numeric(diff(datetime, unit = 'secs'))]>10
+      
+      # if(any(drop_line_check)==TRUE){
+        # drop_line <- min(which(drop_line_check))
+      # }
+      
+      # if(exists("drop_line")){raw_data <- raw_data[(drop_line+1):nrow(raw_data)]}
+      
+      if (identical(meta_data[V1 %like% "v2.0", V1],character(0))){
+        software_version <- "unknown_software_vers"
+      }else{
+        software_version <- meta_data[V1 %like% "v2.0", V1]
+      }
+      
+      meta_data <- data.table(
+        download_date = meta_data[V1=='Download Date:', mdy(V2, tz=tz)],
+        file = file,
+        qa_date = as.Date(Sys.Date()),
+        neph_slope = sensor_data[sensor=='Nephelometer', slope],
+        neph_offset = sensor_data[sensor=='Nephelometer', offset],
+        serial = meta_data[V1=='Device Serial #:', V2],
+        hardware_dt = meta_data[V1=='Date/Time Hardware:', mdy(V2, tz=tz)],
+        software_dt = meta_data[V1=='Date/Time Software:', dmy(V2, tz=tz)],
+        software_vers = software_version,
+        pid = strsplit(basename(file), "_")[[1]][1],
+        loc = strsplit(basename(file), "_")[[1]][2],
+        study_phase = strsplit(basename(file), "_")[[1]][3],
+        filter_id = meta_data[V1=='Filter ID#:', V2],
+        filter_id2 =  strsplit(basename(file), "_")[[1]][5], #use file name filter id
+        sampling_mode = 
+          if(meta_data_dutycycle[V1=='System Times', V2] == "No cycling - Always On"){
+            1
+          }else{
+            on_dc <- meta_data_dutycycle[V1=='System Times', as.numeric(V2)]
+            off_dc <- meta_data_dutycycle[V1=='System Times', as.numeric(V3)]
+            (on_dc/(on_dc + off_dc))
+          }
+        #shutdown_reason = raw_data[shutdown_reason!='', paste(unique(shutdown_reason), collapse=",")]
+        # shutdown_reason = tail(raw_data[!is.na(shutdown_reason) & shutdown_reason != '', shutdown_reason],1)
+      )
+      
+      shutdown_reason_chk <- raw_data[shutdown_reason!='', paste(unique(shutdown_reason), collapse=",")]
+      if(grepl('Duration Stop', shutdown_reason_chk)){shutdown_reason_chk <- "Duration Stop"}
+      meta_data[, shutdown_reason:=shutdown_reason_chk]
+      
+      if(all(output=='meta_data')){return(meta_data)}else
+        if(all(output=='raw_data')){return(raw_data)}else
+          if(all(output == c('raw_data', 'meta_data'))){
+            return(list(meta_data=meta_data, raw_data=raw_data))
+          }
+    }    
+  }
+}
+
 ecm_qa <- function(file, setShiny=TRUE){
-  ingest <- ecm_ingest(file, shiny=setShiny, output=c('raw_data', 'meta_data'))
+  ingest <- ecm_ingest_v2(file, shiny=setShiny, output=c('raw_data', 'meta_data'))
   
   if(is.null(ingest)){return(NULL)}else{
     
@@ -319,15 +506,18 @@ ecm_qa <- function(file, setShiny=TRUE){
       percent_flow_deviation <- round(raw_data[!(flow %between% flow_thresholds), length(flow)]/raw_data[flow %between% flow_thresholds, length(flow)],2)
       n_shutdown_reasons <- sum(raw_data[,shutdown_reason %like% "Flow blocked"]) #Flag if more than 60 instances of flow blocked (five minutes equivalent)
       # flow_flag <- if(percent_flow_deviation>flow_cutoff_threshold | is.nan(percent_flow_deviation) | flow_min<flow_min_threshold | flow_max>flow_max_threshold | n_shutdown_reasons>60 | flow_missing_percent == 100){1}else{0}
-      flow_flag <- if(percent_flow_deviation>flow_cutoff_threshold | is.nan(percent_flow_deviation) |n_shutdown_reasons>60 | flow_missing_percent == 100){1}else{0}
+      # flow_flag <- if(percent_flow_deviation > flow_cutoff_threshold | is.nan(percent_flow_deviation) |n_shutdown_reasons>60 | flow_missing_percent == 100){1}else{0}
+      flow_flag <- if(percent_flow_deviation > flow_cutoff_threshold){1}else{0}
       
       
       #sample duration
       sample_duration <- raw_data_long[variable=='rh_cor_neph' & !is.na(value), as.numeric(difftime(max(datetime), min(datetime), units='mins'))]
       sample_duration_flag <- if(sample_duration %between% sample_duration_thresholds){0}else{1}
-      datetime_start <- min(raw_data_long$datetime)
-      datetime_end <- max(raw_data_long$datetime)
-      
+      # datetime_start <- min(raw_data_long$datetime)
+      # datetime_end <- max(raw_data_long$datetime)
+      datetime_start <- raw_data_long[variable=='rh_cor_neph' & !is.na(value), min(datetime)]
+      datetime_end <- raw_data_long[variable=='rh_cor_neph' & !is.na(value), max(datetime)]
+
       #Wearing compliance
       # raw_data$unique_min <- floor_date(raw_data$datetime, unit = "minute")
       raw_data[, unique_min := floor_date(datetime, unit = "minute")]
