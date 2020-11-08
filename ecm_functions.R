6f0a4db3699a4b03a60a0a155c31a3f028c7de07
Ajay Pillarisetti <ajaypillarisetti@gmail.com>
2020-11-08 08:56:13 -0500

    is.na(condition) change

--- ecm_functions.R
+++ ecm_functions.R
@@ -473,7 +473,7 @@ ecm_qa <- function(file, setShiny=TRUE){
         variable=='inlet_pres' & 
         !is.na(value), 
         quantile(as.numeric(value), 0.95, na.rm = T)]
-      if(is.numeric(inletp_flag_condition)){
+      if(!is.na(inletp_flag_condition)){
         inletp_flag <- if(inletp_flag_condition > inlet_pressure_threshold){1}else{0}
       }else{
         inletp_flag <- NA
