d53dd046a4933aaacf0257509f5d1471dce76b5d
Ajay Pillarisetti <ajaypillarisetti@gmail.com>
2020-11-08 22:20:41 -0500

    change percent_flow_deviation catch for rwanda
    add NA case

--- ecm_functions.R
+++ ecm_functions.R
@@ -512,7 +512,7 @@ ecm_qa <- function(file, setShiny=TRUE){
       n_shutdown_reasons <- sum(raw_data[,shutdown_reason %like% "Flow blocked"]) #Flag if more than 60 instances of flow blocked (five minutes equivalent)
       # flow_flag <- if(percent_flow_deviation>flow_cutoff_threshold | is.nan(percent_flow_deviation) | flow_min<flow_min_threshold | flow_max>flow_max_threshold | n_shutdown_reasons>60 | flow_missing_percent == 100){1}else{0}
       # flow_flag <- if(percent_flow_deviation > flow_cutoff_threshold | is.nan(percent_flow_deviation) |n_shutdown_reasons>60 | flow_missing_percent == 100){1}else{0}
-      flow_flag <- if(percent_flow_deviation > flow_cutoff_threshold){1}else{0}
+      flow_flag <- if(is.na(percent_flow_deviation)){NA}else if(percent_flow_deviation > flow_cutoff_threshold){1}else{0}
       
       
       #sample duration
