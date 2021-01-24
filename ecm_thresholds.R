ed5b7ee00b23d322228d2dbce0ec5c0810b8b3f1
Ricardo Piedrahita <ricpie@gmail.com>
2021-01-24 10:44:58 -0700

    Update ecm_thresholds.R
    added some thresholds for nephelometer data and outlier detection

--- ecm_thresholds.R
+++ ecm_thresholds.R
@@ -12,8 +12,17 @@ flow_max_threshold <- 0.50	#Cease usage, somewhat redundant and unnecessary
 
 bl_shift_threshold <- 30
 
-neph_neg_threshold <- 20
+neph_neg_threshold <- 10
 neph_neg_magnitude_threshold <- -20
+saturation_neph <- 9000
+large_negative_threshold <- -50
+
+#Depositions to corroborate with BC data, or photos of filters
+large_negative_threshold_mg = -.01 #Values of micrograms (depositions) (10µg).  This value is approx the detection limit (3*sd of blanks)
+large_positive_threshold_mg = .200
+bc_outlier_threshold_µg = 10 #only excludes grav value if BC is less than this value (unit of µg), and grav value is greater than .2mg.
+bc_large_positive_threshold_µg = 100 #Values greater than this will be excluded - there are no values above 56 in bl p1 p2.
+
 
 inlet_pressure_threshold <- 5
 
@@ -24,3 +33,5 @@ rh_threshold <- 85
 accel_compliance_threshold <- 0.02 #Compliance flag
 window_width <- 20
 overall_compliance_threshold <- 0.2 #If more than this fraction of samples is not categorize as being compliant, run is flagged.  Includes nighttime data.
+
+
